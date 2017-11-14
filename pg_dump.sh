c566d6a81744dd1395549e115d70476017aaf48a
Vlad Carballo <vcarballo@xogrp.com>
2017-11-14 15:59:40 -0500

    Update pg_dump to encrypt the output backup file with a public key

--- pg_dump.sh
+++ pg_dump.sh
@@ -7,21 +7,14 @@ obsolete_filename=backup_$(date --date="1 day ago" +"%Y%m%d")
 
 pg_dump \
   --no-password | bzip2 | openssl smime -encrypt -aes256 -binary \
-  -outform DEM -out /root/$filename.sql.bz2.ssl /root/rds_backup_key.pem.pub
+  -outform DEM -out /root/$filename.sql.bz2.ssl /etc/secret-volume/ssh-publickey
 
 echo "Backup finished."
 
-ls -la /root
-ls -la /root/.config/rclone
-cat /root/.config/rclone/rclone.conf
-
 echo "Copying backup to GDrive."
 
-which rclone
-
-/usr/bin/rclone lsl remote:/ -vv
-
-/usr/bin/rclone copy /root/entrypoint.sh remote:$PGDATABASE/ -vv
-# rm /root/$filename
+rm /root/$filename
+rclone copy /root/$filename.sql.bz2.ssl remote:$PGDATABASE/ -vv
+rclone delete remote:$PGDATABASE/$obsolete_filename.sql.bz2.ssl -vv
 
 echo "Backup copied to GDrive."
